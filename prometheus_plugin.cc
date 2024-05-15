b9f98eade8ac47b0f66c3eb3847133010601c070
Jacob McSwain <jacob@mcswain.dev>
2024-05-15 02:34:16 -0500

    cmake: minimize

--- prometheus_plugin.cc
+++ prometheus_plugin.cc
@@ -4,7 +4,6 @@
 #include "trunk-recorder/source.h"
 #include "trunk-recorder/plugin_manager/plugin_api.h"
 #include <trunk-recorder/json.hpp>
-#include "trunk-recorder/gr_blocks/decoder_wrapper.h"
 #include <boost/dll/alias.hpp> // for BOOST_DLL_ALIAS
 #include <boost/log/trivial.hpp>
 
